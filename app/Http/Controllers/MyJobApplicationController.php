54cc4c1b14e52c3eccc06f0d53d1296488033a0d
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2025-01-31 12:16:59 +0100

    feat: Cancel/ Delete Job Application

--- app/Http/Controllers/MyJobApplicationController.php
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\JobApplication;
 
 class MyJobApplicationController extends Controller
 {
@@ -20,8 +21,10 @@ class MyJobApplicationController extends Controller
        return view("my_job_application.index", ["applications" => $applications]);
     }
 
-    public function destroy(string $id)
+    public function destroy(JobApplication $myJobApplication)
     {
-        //
+        $myJobApplication->delete();
+
+        return redirect()->back()->with("success", "Job application removed");
     }
 }
--- resources/views/my_job_application/index.blade.php
+++ resources/views/my_job_application/index.blade.php
@@ -21,12 +21,23 @@
                 </div>
 
                 <div>
-                    Cancel
+                    <form action="{{ route('my-job-applications.destroy', $application )}}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <x-button>Cancel</x-button>
+                    </form>
                 </div>
             </div>
         </x-job-card>
     @empty
-        
+        <div class="rounded-md border border-dashed border-slate-300 p-8">
+            <div class="text-center fond-medium">
+                No Job application yet
+            </div>
+            <div class="text-center">
+                Go find some jobs <a href="{{ route("jobs.index")}}" class="text-indigo-500 hover:underline">HERE</a>
+            </div>
+        </div>
     @endforelse
     
 </x-layout>
