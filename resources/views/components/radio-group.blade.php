6f2c918483e16866640f33c98d19a5e69b114e14
MiljanSjekloca01 <miljan.sjekloca.20@singimail.rs>
2025-02-04 14:24:05 +0100

    fix: adjust radio button checked value logic

--- resources/views/components/radio-group.blade.php
+++ resources/views/components/radio-group.blade.php
@@ -1,11 +1,15 @@
+@props(['name', 'options', 'showAllOption' => true])
 <div>
-    <label for="{{ $name }}_all" class="mb-1 flex items-center" >
-        <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) id="{{$name}}_all" />       
-        <span class="ml-2" >All</span>
-    </label>
+    @if ($showAllOption)
+        <label for="{{ $name }}_all" class="mb-1 flex items-center" >
+            <input type="radio" name="{{ $name }}" value="" @checked(!request($name)) id="{{$name}}_all" />       
+            <span class="ml-2" >All</span>
+        </label>
+    @endif
     @foreach ($options as $option)
         <label for="{{ $name }}_{{ $option }}" class="mb-1 flex items-center" >
-            <input type="radio" name="{{ $name }}" value="{{ $option }}" @checked($option === request($name)) id="{{ $name }}_{{$option}}" />
+            <input type="radio" name="{{ $name }}" value="{{ $option }}" id="{{ $name }}_{{$option}}" 
+             @checked($option === ($value ?? request($name)) ) />
             <span class="ml-2" >{{Str::ucfirst($option)}}</span>
         </label>
     @endforeach
